a7bbf8ce8dd9eb7493c1f0e191b6216995947c16
alitajalli <51313120+alitajalli@users.noreply.github.com>
2021-10-06 00:01:54 +0330

    Update website-config.ts

--- src/website-config.ts
+++ src/website-config.ts
@@ -59,16 +59,16 @@ const config: WebsiteConfig = {
   coverImage: 'img/blog-cover.png',
   logo: 'img/greengarnet.png',
   lang: 'en',
-  siteUrl: 'https://gatsby-casper.netlify.com',
-  facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
+  siteUrl: 'https://greengarnet.org',
+  facebook: 'https://www.facebook.com/greengarnet.official',
+  twitter: 'https://twitter.com/ali19364317',
   showSubscribe: true,
   mailchimpAction:
     'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
   mailchimpEmailFieldName: 'MERGE0',
   googleSiteVerification: 'GoogleCode',
-  footer: 'is based on Green Garnet developer team',
+  footer: 'Contact Us: +98 990 1377 600 ',
   showAllTags: true,
 };
 

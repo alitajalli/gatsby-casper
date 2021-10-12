9ebca8d350ee23eaddbe9cb0620073c66225a227
alitajalli <51313120+alitajalli@users.noreply.github.com>
2021-10-12 13:41:59 +0330

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
+import { StaticImage } from "gatsby-plugin-image"
+
 import { css } from '@emotion/react';
 
 import { Footer } from '../components/Footer';
@@ -81,7 +83,7 @@ const About: React.FC = () => (
                   It can also boost the respiratory and the immune systems and protect you against highly infectious diseases.
                   The healing energies of Green-Garnet can detoxify the body and reduce inflammations
                 </p>
-                <img src="img/garnet_coin1.jpg" />
+                <StaticImage src="img/garnet_coin1.jpg" alt="A dinosaur" />
                 <p>
                   Green-Garnet is the gemstone that will confirm the happiness that you have in your love life.
                   It will show you how you can appreciate the people you love and the people who love you back.

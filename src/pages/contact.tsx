414c06795c1e5029074bfeb9028b608a6966f11d
alitajalli <51313120+alitajalli@users.noreply.github.com>
2021-10-17 01:34:18 +0330

    Update contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -60,43 +60,39 @@ const About: React.FC = () => (
                   <a > Meanings, Properties and Powers</a>
                 </h5>
                 <p>
-                  Green-Garnet is restorative and liberating. It’s a growth stone that is also a powerful conduit of birth, creation, development, and renewal.
-                  It’s a powerful aid in nurturing a fledgling relationship or a new business endeavor. It will also promote a strengthened commitment to your 
-                  higher purpose and spiritual growth.
-                  It will bring new strength when you are experiencing a difficult time or a challenging period in your life.
-                  It’s a stone that will cleanse the heart chakra, which will increase your physical vitality and zest for life. It will also induce feelings 
-                  of kindness, compassion, and charity.
-                  Beautiful Green Garnet beads
-                  Green-Garnet is a known gemstone that has the power of regeneration and growth.
-                  Its beautiful green color is a symbol of Mother Earth.
-                  It’s believed that Green-Garnet has the power to accept, care for, and nurture everything!
-                  It’s a stone that’s brimming with hope, and its powerful energies will definitely brighten your future!
-                  Green-Garnet symbolizes growth and renewal in the different aspects of your life.
+                  Jewelry is gorgeous ! Unfortunately, it can be difficult to take what your imagination creates and turn it into an actual product and price too high to do marketing. That is why you should consider Green Garnet’s professional CAD / mass order services .
+
+المجوهرات رائعة! لسوء الحظ ، قد يكون من الصعب أن تأخذ ما يبتكره خيالك وتحويله إلى منتج فعلي وسعر مرتفع جدًا للقيام بالتسويق ، ولهذا السبب يجب عليك التفكير في خدمات CAD / الطلبات الجماعية الاحترافية من Green Garnet 
+
+                  I gioielli sono stupendi! Sfortunatamente, può essere difficile prendere ciò che la tua immaginazione crea e trasformarlo in un prodotto reale e un prezzo troppo alto per fare marketing. Ecco perché dovresti prendere in considerazione i servizi CAD / ordini di massa professionali di Green Garnet.
+
                 </p>
                 <p>
-                  Green-Garnet also has physical healing properties that can benefit the body in more ways than one.
-                  This gemstone is known to be very effective in speeding up the recovery period after an illness or physical trauma.
-                  It can stimulate blood flow and help in new cell growth, especially when you pair it with the April Birthstone.
-                  It can also boost the respiratory and the immune systems and protect you against highly infectious diseases.
-                  The healing energies of Green-Garnet can detoxify the body and reduce inflammations
+                 Our CAD services allow jewelry online/offline stores and jewelry designers to think outside the box when it comes to creating custom jewelry series. No longer are they bound by what they can physically create by hand. Instead, a computer software program can help turn a creative and innovative design concept into reality.
+                  Fast, Quick and Reliable Services from Green Garnet
+                  To get started, contact your job coordinator today.
+الخاصة بنا لمتاجر المجوهرات عبر الإنترنت / غير المتصلة بالإنترنت ومصممي المجوهرات بالتفكير خارج الصندوق عندما يتعلق الأمر بإنشاء سلسلة مجوهرات مخصصة. لم يعدوا ملزمين بما يمكنهم صنعه يدويًا. بدلاً من ذلك ، يمكن أن يساعد برنامج الكمبيوتر في تحويل مفهوم التصميم الإبداعي والمبتكر إلى واقع ملموس.
+خدمات سريعة وسريعة وموثوقة من مجوهرات Green Garnet
+للبدء ، اتصل بمنسق عملك اليوم.
+                I nostri servizi CAD consentono ai negozi di gioielli online/offline e ai designer di gioielli di pensare fuori dagli schemi quando si tratta di creare serie di gioielli personalizzati. Non sono più vincolati da ciò che possono creare fisicamente a mano. Invece, un programma software per computer può aiutare a trasformare in realtà un concetto di design creativo e innovativo.
+                Servizi veloci, rapidi e affidabili da Green Garnet
+                Per iniziare, contatta oggi stesso il tuo coordinatore del lavoro.
+
                 </p>
+                    <StaticImage src="../content/img/garnet_coin1.jpg" alt="A dinosaur" />
                 <p>
-                  Green-Garnet is the gemstone that will confirm the happiness that you have in your love life.
-                  It will show you how you can appreciate the people you love and the people who love you back.
-                  It also has the ability to strengthen not only the heart but the soul as well. It will help you take in new energies and become whole again 
-                  or a new and better version of yourself.
-                  If you want to make a new beginning, Green-Garnet is the perfect gemstone for you!
-                  Green-Garnet also symbolizes hope. It will give you clarity and help you see things or people in a different light.
-                  It will solidify new relationships and add new meaning to your life and your relationship.
-                  When you work with the energies of this stone, you will feel courageous and confident to make big changes and major decisions.
-                  It’s a gemstone that will encourage gratitude. It will make you appreciate all the good things that you have going for you.
-                  It will even make you thankful for the bad, because without them, you will never realize just how blessed you are!
-                  Green-Garnet will inspire you to be of service to the people you love. You will do things for them without expecting anything in return.
-                  You will do it simply because you love them and you just want to see them happy and okay.
-                  This gemstone will promote a relaxed and laidback attitude in your relationship. It will encourage you to go with the flow and not to be too rigid 
-                  in your ways.
-                  Green-Garnet will inspire you to inject good old-fashioned romance and silly fun into a serious relationship.
-                  It will remind you to never lose passion or energy for one another, especially during times when you’re filled with stress, fear, or worry.
+                  Designing jewelry from home can be fun and rewarding. Through Jewelry Designs’ virtual jewelry service, you will have direct access to a designer and a personal shopper.
+
+                  ## 1. Chat With A Representative:
+                     One of our designers will be with you shortly.From there, we will schedule a call or arrange a meeting to get started.
+
+                  ## 2. Actualize Your Concept:
+                     Your jewelry designer will begin by establishing your needs and gathering relevant materials related to your project. From there, they will develop your idea and communicate with you using photos, computer generated renderings, and various design tools available to help you visualize your masterpiece.
+
+                  ## 3. Create Your Jewelry:
+                    Once we have earned your complete confidence, we will customize a piece of fine jewelry to your specifications.
+
+                  Whatsapp: +989901377600
                 </p>
               </div>
             </PostFullContent>

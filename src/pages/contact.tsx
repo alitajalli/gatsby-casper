27a8240db1de69d1a72c8e8e675f18ee121974d0
alitajalli <51313120+alitajalli@users.noreply.github.com>
2021-10-17 01:45:39 +0330

    Update contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -83,13 +83,13 @@ const About: React.FC = () => (
                 <p>
                   Designing jewelry from home can be fun and rewarding. Through Jewelry Designs’ virtual jewelry service, you will have direct access to a designer and a personal shopper.
 
-                  ## 1. Chat With A Representative:
+                 1. Chat With A Representative:
                      One of our designers will be with you shortly.From there, we will schedule a call or arrange a meeting to get started.
 
-                  ## 2. Actualize Your Concept:
+                  2. Actualize Your Concept:
                      Your jewelry designer will begin by establishing your needs and gathering relevant materials related to your project. From there, they will develop your idea and communicate with you using photos, computer generated renderings, and various design tools available to help you visualize your masterpiece.
 
-                  ## 3. Create Your Jewelry:
+                  3. Create Your Jewelry:
                     Once we have earned your complete confidence, we will customize a piece of fine jewelry to your specifications.
 
                   Whatsapp: +989901377600

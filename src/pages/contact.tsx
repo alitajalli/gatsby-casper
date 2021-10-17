4a039b8ce843679d14fa090084bfb5a759094c6c
alitajalli <51313120+alitajalli@users.noreply.github.com>
2021-10-17 18:45:29 +0330

    Update contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -108,6 +108,7 @@ Once we have earned your complete confidence, we will customize a piece of fine 
                 </p>
                 <p> 
                 whatsapp: +989901377600
+                  8th floor, Shiraz University Management building, Jomhori blv, Shiraz, Fars Province, Iran
                 </p>
                 
               </div>

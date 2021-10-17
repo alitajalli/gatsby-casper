724edd21425a8f3213eaa530f6f0bad215638165
alitajalli <51313120+alitajalli@users.noreply.github.com>
2021-10-17 12:21:40 +0330

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -62,6 +62,7 @@ const About: React.FC = () => (
                   Green Garnet <br /> Inspired by:{' '}
                   <a > Meanings, Properties and Powers</a>
                 </h5>
+                 <StaticImage src="../content/img/greengarnetlogos2.jpg" alt="A dinosaur" />
                 <p>
                   Green-Garnet is restorative and liberating. It’s a growth stone that is also a powerful conduit of birth, creation, development, and renewal.
                   It’s a powerful aid in nurturing a fledgling relationship or a new business endeavor. It will also promote a strengthened commitment to your 
@@ -69,6 +70,9 @@ const About: React.FC = () => (
                   It will bring new strength when you are experiencing a difficult time or a challenging period in your life.
                   It’s a stone that will cleanse the heart chakra, which will increase your physical vitality and zest for life. It will also induce feelings 
                   of kindness, compassion, and charity.
+                  </p>
+                <StaticImage src="../content/img/green-garnet_Header-Banner.jpg" alt="A dinosaur" />
+                  <p>
                   Beautiful Green Garnet beads
                   Green-Garnet is a known gemstone that has the power of regeneration and growth.
                   Its beautiful green color is a symbol of Mother Earth.
@@ -76,6 +80,7 @@ const About: React.FC = () => (
                   It’s a stone that’s brimming with hope, and its powerful energies will definitely brighten your future!
                   Green-Garnet symbolizes growth and renewal in the different aspects of your life.
                 </p>
+                <StaticImage src="../content/img/tsavorite-aaa.jpg" alt="A dinosaur" />
                 <p>
                   Green-Garnet also has physical healing properties that can benefit the body in more ways than one.
                   This gemstone is known to be very effective in speeding up the recovery period after an illness or physical trauma.
@@ -83,12 +88,14 @@ const About: React.FC = () => (
                   It can also boost the respiratory and the immune systems and protect you against highly infectious diseases.
                   The healing energies of Green-Garnet can detoxify the body and reduce inflammations
                 </p>
-                <StaticImage src="../content/img/garnet_coin1.jpg" alt="A dinosaur" />
-                <p>
+                
+                 <p>
                   Green-Garnet is the gemstone that will confirm the happiness that you have in your love life.
                   It will show you how you can appreciate the people you love and the people who love you back.
                   It also has the ability to strengthen not only the heart but the soul as well. It will help you take in new energies and become whole again 
                   or a new and better version of yourself.
+                  </p>
+                  <p>
                   If you want to make a new beginning, Green-Garnet is the perfect gemstone for you!
                   Green-Garnet also symbolizes hope. It will give you clarity and help you see things or people in a different light.
                   It will solidify new relationships and add new meaning to your life and your relationship.
@@ -99,6 +106,9 @@ const About: React.FC = () => (
                   You will do it simply because you love them and you just want to see them happy and okay.
                   This gemstone will promote a relaxed and laidback attitude in your relationship. It will encourage you to go with the flow and not to be too rigid 
                   in your ways.
+                  </p>
+                <StaticImage src="../content/img/garnet_coin1.jpg" alt="A dinosaur" />
+                  <p>
                   Green-Garnet will inspire you to inject good old-fashioned romance and silly fun into a serious relationship.
                   It will remind you to never lose passion or energy for one another, especially during times when you’re filled with stress, fear, or worry.
                 </p>
